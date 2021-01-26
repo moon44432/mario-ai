f07de4e053953ef5c52b191e61a52efe44b0dea4
Moon Seung-gi <moon44432@naver.com>
2021-01-26 17:22:46 +0900

    initial commit

---
+++ network.py
@@ -0,0 +1,68 @@
+
+from tensorflow.keras.layers import Activation, Add, BatchNormalization, Conv2D, Dense, GlobalAveragePooling2D, Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras import backend as K
+import os
+
+# params
+dn_filters = 32
+dn_residual_num = 8
+img_width = 128
+key_num = 4
+
+DN_INPUT_SHAPE = (img_width, img_width, 2)
+DN_OUTPUT_SIZE = key_num
+
+
+def conv(filters):
+    return Conv2D(filters, 3, padding='same', use_bias=False,
+                  kernel_initializer='he_normal', kernel_regularizer=l2(0.0005))
+
+
+def residual_block():
+    def f(x):
+        sc = x
+        x = conv(dn_filters)(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        x = conv(dn_filters)(x)
+        x = BatchNormalization()(x)
+        x = Add()([x, sc])
+        x = Activation('relu')(x)
+        return x
+
+    return f
+
+
+def network():
+    if os.path.exists('./model/model.h5'):
+        return
+
+    input = Input(shape=DN_INPUT_SHAPE)
+
+    x = conv(dn_filters)(input)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    for i in range(dn_residual_num):
+        x = residual_block()(x)
+
+    x = GlobalAveragePooling2D()(x)
+
+    p = Dense(DN_OUTPUT_SIZE, kernel_regularizer=l2(0.0005),
+              activation='softmax', name='pi')(x)
+
+    v = Dense(1, kernel_regularizer=l2(0.0005))(x)
+    v = Activation('tanh', name='v')(v)
+
+    model = Model(inputs=input, outputs=[p, v])
+
+    os.makedirs('./model/', exist_ok=True)
+    model.save('./model/model.h5')
+
+    K.clear_session()
+    del model
+
+if __name__ == '__main__':
+    network()
---
+++ process_image.py
@@ -0,0 +1,29 @@
+
+import numpy as np
+import cv2
+from mss import mss
+
+mon = {'top': 60, 'left': 0, 'width': 512, 'height': 480}
+
+def process_img(original_image):
+    bw_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    resized_img = cv2.resize(bw_img, dsize=(128, 128))
+    return resized_img
+
+def get_image():
+    with mss() as sct:
+        screen = np.array(sct.grab(mon))
+        return screen
+
+
+if __name__ == "__main__":
+    with mss() as sct:
+        while True:
+            screen = get_image()
+            new_screen = process_img(screen)
+
+            cv2.imshow('mario', new_screen)
+
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                break
